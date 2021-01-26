c88a879121a107707473b1fabece792fc035a8ac
Nick Playfair <nick@playfair.dev>
2021-01-26 23:39:35 +0000

    Image processing
    Use pcb-stackup to generate svg data from gerber files and then use sharp to generate a png file from the svg buffer

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const fs = require('fs');
-const pcbStackup = require('pcb-stackup');
+const fs          = require('fs');
+const pcbStackup  = require('pcb-stackup');
+const sharp       = require('sharp');
+
+// Configuration
+const resizeWidth = 600;
+const density = 1000;
+const compLevel = 1;
+const destfile = './gerber/pcb3.png';
 
 async function writeSVG(filepath, data) {
   try {
@@ -10,15 +17,11 @@ async function writeSVG(filepath, data) {
 }
 
 const fileNames = [
-  './gerber/default/copper_bottom.gbr',
   './gerber/default/copper_top.gbr',
   './gerber/default/drill_1_16.xln',
-  './gerber/default/silkscreen_bottom.gbr',
   './gerber/default/silkscreen_top.gbr',
-  './gerber/default/soldermask_bottom.gbr',
   './gerber/default/soldermask_top.gbr',
   './gerber/default/solderpaste_top.gbr',
-  './gerber/default/solderpaste_bottom.gbr',
   './gerber/default/profile.gbr',
 ]
 
@@ -28,5 +31,19 @@ const layers = fileNames.map(filename => ({
 }));
 
 pcbStackup(layers).then(stackup => {
-  writeSVG('./gerber/pcb.svg', stackup.top.svg);
+  // Create buffer from SVG string
+  sharp(Buffer.from(stackup.top.svg), { density: density })
+  .resize({ width: resizeWidth })
+  .png({ 
+    compressionLevel: compLevel })
+  .toFile(destfile)
+  .then((info) => {
+    console.log(info)
+  })
+  .catch((e) => {
+    console.error(e);
+  })
+})
+.catch((e) => {
+  console.error(e);
 });
