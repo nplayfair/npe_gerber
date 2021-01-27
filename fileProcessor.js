a1b06686fe5bf90150d9b4454ef5223b25377e87
Nick Playfair <nick@playfair.dev>
2021-01-27 20:18:30 +0000

    Start working on file processing functions

---
+++ fileProcessor.js
@@ -0,0 +1,156 @@
+const StreamZip = require('node-stream-zip');
+const fs = require('fs-extra');
+const path = require('path');
+
+// Filenames we need to extract from the archive
+const gerberFiles = [
+  'CAMOutputs/DrillFiles/drills.xln',
+  'CAMOutputs/GerberFiles/copper_top.gbr',
+  'CAMOutputs/GerberFiles/silkscreen_top.gbr',
+  'CAMOutputs/GerberFiles/soldermask_top.gbr',
+  'CAMOutputs/GerberFiles/solderpaste_top.gbr',
+  'CAMOutputs/GerberFiles/profile.gbr'
+]
+
+let newLayers = [];
+
+// Use a sample zip archive
+
+const zip =  new StreamZip({
+  file: './gerber/sho_v2.zip',
+  storeEntries: true
+});
+
+function countFiles() {
+  // Read the zip file
+  zip.on('ready', () => {
+    console.log('Entries read: ' + zip.entriesCount);
+    for (const entry of Object.values(zip.entries())) {
+      const desc = entry.isDirectory ? 'directory' : `${entry.size} bytes`;
+      console.log(`Entry ${entry.name}: ${desc}`);
+    }
+    zip.close()
+  });
+}
+
+function getFiles() {
+  // Get the gerber files from the zip archive
+  zip.on('ready', () => {
+    let fileName = 'CAMOutputs/GerberFiles/silkscreen_top.gbr';
+    zip.stream(fileName, (err, stm) => {
+      newLayers.push({
+        filename: fileName,
+        gerber: stm
+      });
+      stm.on('end', () => zip.close());
+      console.log(newLayers);
+    });
+  });
+}
+
+async function getLayers2(fileName) {
+  const tempDir = path.join(__dirname, 'gerber', 'tmp', 'archive');
+  const archive =  new StreamZip({
+    file: fileName,
+    storeEntries: true
+  });
+  try {
+    archive.on('ready', () => {
+      fs.mkdirSync(tempDir, { recursive: true });
+      archive.extract(null, tempDir, (err, count) => {
+        console.log(err ? 'Extract error' : `Extracted ${count} entries`);
+        const layers = gerberFiles.map(fileName => ({
+          filename: fileName,
+          gerber: fs.createReadStream(path.join(tempDir, fileName))
+        }));
+        archive.close();
+        return layers;
+      });
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function getLayers(fileName) {
+  try {
+    const tempDir = path.join(__dirname, 'gerber', 'tmp', 'archive');
+    extractArchive(fileName)
+      .then(numfiles => {
+        // const layers = gerberFiles.map(fileName => ({
+        //   filename: fileName,
+        //   gerber: fs.createReadStream(path.join(tempDir, fileName))
+        // }));
+        // console.log(layers);
+        console.log(`${numfiles} files extracted successfully`);
+      })
+      .catch(e => {
+        console.log(e);
+      })
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function extractArchive(fileName) {
+  // Configure archive to use
+  const archive =  new StreamZip({
+    file: fileName,
+    storeEntries: true
+  });
+  return new Promise((resolve, reject) => {
+    // Try to extract
+    archive.on('ready', () => {
+      let extDir = path.join(__dirname, 'gerber', 'tmp', 'archive');
+      fs.mkdirSync(extDir, { recursive: true });
+      archive.extract(null, extDir, (err, count) => {
+        if(!err) {
+          archive.close();
+          resolve(count);
+        } else {
+          const errMsg = 'Error extracting archive';
+          console.err(errMsg);
+          archive.close();
+          reject(errMsg)
+        }
+      })
+    })
+  })
+}
+
+// Helper methods
+function validFiles(file) {
+  return gerberFiles.includes(file);
+}
+
+function layerBuild(filename) {
+  fs.createReadStream(filename);
+}
+
+async function cleanupFiles() {
+  try {
+    let folder = path.join(__dirname, 'gerber', 'tmp');
+    await fs.emptyDirSync(folder);
+    console.log('Temp files removed.');
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function getStream(fileName) {
+  let stream;
+  zip.on('ready', () => {
+    stream = zip.entryDataSync(fileName);
+      zip.close();
+  console.log(stream);
+  })
+}
+
+
+// We want to return an array of layers
+
+
+exports.countFiles = countFiles;
+exports.extractArchive = extractArchive;
+exports.cleanupFiles = cleanupFiles;
+exports.getLayers = getLayers;
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs          = require('fs');
+const { file } = require('jszip');
 const pcbStackup  = require('pcb-stackup');
 const sharp       = require('sharp');
+const fileProc    = require('./fileProcessor.js');
 
 // Configuration
 const resizeWidth = 600;
@@ -9,7 +11,7 @@ const compLevel = 1;
 const destfile = './gerber/pcb3.png';
 
 //Sample gerber
-const fileNames = [
+const sampleNames = [
   './gerber/default/copper_top.gbr',
   './gerber/default/drill_1_16.xln',
   './gerber/default/silkscreen_top.gbr',
@@ -18,25 +20,44 @@ const fileNames = [
   './gerber/default/profile.gbr',
 ]
 
-const layers = fileNames.map(filename => ({
+const layers = sampleNames.map(filename => ({
   filename,
   gerber: fs.createReadStream(filename),
 }));
 
-pcbStackup(layers).then(stackup => {
-  // Create buffer from SVG string
-  sharp(Buffer.from(stackup.top.svg), { density: density })
-  .resize({ width: resizeWidth })
-  .png({ 
-    compressionLevel: compLevel })
-  .toFile(destfile)
-  .then((info) => {
-    console.log(info)
-  })
-  .catch((e) => {
-    console.error(e);
-  })
-})
-.catch((e) => {
-  console.error(e);
-});
+// pcbStackup(layers).then(stackup => {
+//   // Create buffer from SVG string
+//   sharp(Buffer.from(stackup.top.svg), { density: density })
+//   .resize({ width: resizeWidth })
+//   .png({ 
+//     compressionLevel: compLevel })
+//   .toFile(destfile)
+//   .then((info) => {
+//     console.log(info)
+//   })
+//   .catch((e) => {
+//     console.error(e);
+//   })
+// })
+// .catch((e) => {
+//   console.error(e);
+// });
+
+// fileProc.jsTest();
+// console.log(layers);
+
+// fileProc.getLayers();
+// fileProc.getStream('CAMOutputs/GerberFiles/silkscreen_top.gbr');
+// fileProc.extractArchive('./gerber/sho_v2.zip')
+// fileProc.cleanupFiles();
+
+// async () => {
+//   const templayers = await fileProc.getLayers('./gerber/sho_v2.zip');
+//   console.log(templayers);
+// }
+// fileProc.extractArchive('./gerber/sho_v2.zip')
+//   .then(msg => {
+//     console.log('done');
+//   })
+
+fileProc.getLayers('./gerber/sho_v2.zip')
